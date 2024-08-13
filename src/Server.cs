ab24b72fc5e69c13242e490587fcb2ed5f490bdc
gso <glenn.s.olesen@gmail.com>
2024-08-13 20:02:16 +0200

    need an easier entres atm

--- src/Server.cs
+++ src/Server.cs
@@ -8,9 +8,5 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
-var client = server.AcceptSocket(); // wait for client
 
-for (int i = 0; i < 2; i++)
-{
-    await client.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"));
-}
+var client = server.AcceptSocket(); // wait for client
