06e6af32d209a54e80ccddcc7d975fa65d01ec2c
GSittrup <glenn.s.olesen@gmail.com>
2024-11-29 15:19:51 +0100

    codecrafters submit [skip ci]

---
+++ src/Response/ServerResponse.cs
@@ -0,0 +1,6 @@
+namespace Redis.src.Response;
+
+class ServerResponse
+{
+    public static void Response() { }
+}
--- src/Server.cs
+++ src/Server.cs
@@ -2,11 +2,15 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
 // Uncomment this block to pass the first stage
 TcpListener server = new(IPAddress.Any, 6379);
 server.Start();
 
-var client = server.AcceptSocket(); // wait for client
+var client = server.AcceptSocket();
+
+while (client.Connected) {
+var buffer = new byte[1024];
+
+await client.ReceiveAsync(buffer);
+await client.SendAsync(Encoding.ASCII.GetBytes("+PONG\r\n"));
+}
